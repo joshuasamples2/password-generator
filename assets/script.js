bf9b8de930f83e7a4512ecefae08d3dc5ff9bea3
joshuasamples2 <joshua.samples2@gmail.com>
2021-09-16 20:40:06 -0500

    updated code

--- assets/script.js
+++ assets/script.js
@@ -2,69 +2,107 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+function writePassword() 
+{
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
-function generatePassword(){
-//having arrays of your options
-//asking how many characters
-var length =prompt("How many characters? (8-128)")
-if (length < 8) {
+var upperCase='ABCDEFGHIJKLMNOPQRSTUVWXYZ';  
+var lowerCase='abcdefghijklmnopqrstuvwxyz';  
+var number='1234567890';  
+var special='!@#$%^&*()_+-={}';  
+var usercharacterchoice= ""
+var password = "";
+
+var userlength =prompt("How many characters? (8-128)")
+if (userlength < 8) 
+{
   alert("The password must be at least 8 characters!");
-  return;
 }
-if (length > 128) {
+if (userlength > 128) 
+{
   alert("The password must be less than 128 characters!");
-  return;
 }
-var upperCase=confirm("Do you want to use uppercase letters?")
-var lowerCase=confirm("Do you want to use lowercase letters?")
-var numbers=confirm("Do you want to use numbers?")
-var special=confirm("Do you want to use special characters?")
-
-
-function RandomupperCase(){
- var upperCase='ABCDEFGHIJKLMNOPQRSTUVWXYZ';  
-  return upperCase[Math.floor(Math.random()*upperCase.length)]
-}
-console.log(RandomupperCase());
-
-function RandomlowerCase(){
-  var lowerCase='abcdefghijklmnopqrstuvwxyz';  
-   return lowerCase[Math.floor(Math.random()*lowerCase.length)]
- }
- console.log(RandomlowerCase());
-
- function RandomNumber(){
-  var number='1234567890';  
-   return number[Math.floor(Math.random()*number.length)]
- }
- console.log(RandomNumber());
+var upperCaseChoice=confirm("Do you want to use uppercase letters?")
+var lowerCaseChoice=confirm("Do you want to use lowercase letters?")
+var numbersChoice=confirm("Do you want to use numbers?")
+var specialChoice=confirm("Do you want to use special characters?")
+function generatePassword() 
+{
  
- function RandomSpecial (){
-  var special='!@#$%^&*()_+-={}';  
-   return special[Math.floor(Math.random()*special.length)]
- }
- console.log(RandomSpecial());
+var usercharacterchoice = (!specialChoice && !numbersChoice && !upperCaseChoice && !lowerCaseChoice)
+  if  (specialChoice && numbersChoice && upperCaseChoice && lowerCaseChoice) 
+{
+  usercharacterchoiceices = special.concat(number, lowerCase, upperCase);
+}
+else if (specialChoice && numbersChoice && lowerCaseChoice) 
+{
+  usercharacterchoice = special.concat(number, lowerCase);
+}
+else if (specialChoice && numbersChoice && upperCaseChoice) 
+{
+  usercharacterchoice = special.concat(number, upperCase);
+}
+else if (numbersChoice && lowerCaseChoice && upperCaseChoice) 
+{
+  usercharacterchoice = number.concat(lowerCase, upperCase);
+}
+else if (specialChoice && lowerCaseChoice && upperCaseChoice) 
+{
+  usercharacterchoice = special.concat(lowerCase, upperCase);
+}
+else if (specialChoice && numbersChoice) 
+{
+  usercharacterchoice = special.concat(number);
+} 
+else if (specialChoice && lowerCaseChoice) 
+{
+  usercharacterchoice = special.concat(lowerCase);
+} 
+else if (specialChoice && upperCaseChoice) 
+{
+  usercharacterchoice = special.concat(upperCase);
+}
+else if (lowerCaseChoice && numbersChoice) 
+{
+  usercharacterchoice = lowerCase.concat(number);
+} 
+else if (lowerCaseChoice && upperCaseChoice) 
+{
+  usercharacterchoice = lowerCase.concat(upperCase);
+} 
+else if (numbersChoice && upperCaseChoice) 
+{
+  usercharacterchoice = number.concat(upperCase);
+}
 
-
-
-var choices = [upperCase, lowerCase, numbers, special] 
-
-//grab random character by their index
-//build the password
+else if (specialChoice) 
+{
+  usercharacterchoice = special;
+}
+else if (confirmNumber)
+{
+  usercharacterchoice = number;
+}
+else if (lowerCaseChoice) 
+{
+  usercharacterchoice = lowerCase;
+}
+else if (upperCaseChoice) 
+{
+  usercharacterchoice = upperCase;
+}
+function generatePassword()
+{
+for (var i = 0; i < userlength; i++)
+ {
+ var usercharacterchoice = usercharacterchoice[Math.floor(Math.random()*usercharacterchoice.length)]
+};
+}
 var randomPassword = ''
 randomPassword+='';
-
-//loop up to the number of characters that they picked 
-  return ''
-
-
-
-}
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+generatePassword();
+writePassword();
+console.log(generatePassword)
+}
